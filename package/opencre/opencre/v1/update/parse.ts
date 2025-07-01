7b595791e45775515599d4253aa475ed34dbd710
Daniel Rojas <lrojas@neverfail.com>
2025-07-01 12:21:08 -0600

    chore: move to v1 folder

--- package/opencre/opencre/v1/update/parse.ts
+++ package/opencre/opencre/v1/update/parse.ts
@@ -3,11 +3,14 @@ import { getLogger } from '@auditmation/util-logger';
 import fs from 'fs';
 import yml from 'js-yaml';
 import path from 'path';
+import { cwd } from 'process';
 
 
 const unmapped = new Set<string>();
 const data = require('./all_cres.json').data;
 const logger = getLogger('general');
+
+const elementsLinks = {};
 
 const standardFunctionMappings: Record<string, (x: string) => string> = {
   "NIST 800-53 v5": (x) => `nist.80053.rev5.framework/${x.split(' ')[0].toLowerCase().replace(' ', '').replace('(', '_').replace(')', '')}`,
@@ -30,10 +33,19 @@ interface Document {
 }
 
 
-function linkToPackageCode(document: Document): string {
+function linkToPackageCode(document: Document, elementId: string): string {
   const mapping = standardFunctionMappings[document.name];
   if (mapping) {
-    return mapping(document.sectionID || document.section!);
+    const mappedName = mapping(document.sectionID || document.section || document.name);
+    const [packageName, element] = mappedName.split('/');
+    if (element == undefined) {
+      return mappedName;
+    }
+    if (!elementsLinks[packageName]) {
+      elementsLinks[packageName] = [];
+    }
+    elementsLinks[packageName].push([element, elementId]);
+    return mappedName;
   }
   unmapped.add(document.name);
   return `${document.name}/${document.sectionID || document.section}`
@@ -56,7 +68,9 @@ function main() {
   data.forEach((e: any) => {
     const demonstrates = e.links
       .filter(l => l.document.doctype == 'Standard')
-      .map((l: any) => linkToPackageCode(l.document))
+      .map((l: any) => linkToPackageCode(l.document, e.id))
+
+
     const links = demonstrates.length > 0 ? { demonstrates } : undefined;
     const element = {
       id: UUID.generateV4().toString(),
@@ -70,5 +84,20 @@ function main() {
     saveYML(path.join(__dirname, '../elements', `${element.externalId}.yml`), element);
   })
   logger.info('Unmapped standards:', unmapped);
+  logger.info('Elements links:', elementsLinks);
+  Object.entries(elementsLinks).forEach(([packageName, elements]) => {
+    const packageData = {
+      elements: (elements as [string, string][]).map(([element, elementId]) => ({
+        id: UUID.generateV4().toString(),
+        targetElement: elementId,
+        sourceElement: element,
+        relationshipType: 'intersects',
+        strengthOfRelationship: 5,
+      }))
+    };
+    saveYML(cwd() + `/${packageName}.yml`, packageData);
+  });
+  logger.info('All done!');
+
 }
 main();
